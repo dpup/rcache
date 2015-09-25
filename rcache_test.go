b163f4e17d1aa928f684cf133802973b3ac742e2
Dan Pupius <dan@medium.com>
2015-09-24 23:36:47 -0700

    Update example

--- rcache_test.go
+++ rcache_test.go
@@ -40,20 +40,21 @@ func ExampleCache() {
 	})
 
 	// Original image is only fetched once.
+	tx, _ := c.Get(CarKey{"BMW", "M5"})
 	t200, _ := c.Get(ThumbnailKey{"BMW", "M5", 200})
 	t400, _ := c.Get(ThumbnailKey{"BMW", "M5", 400})
 
-	fmt.Printf("%s + %s", t200, t400)
+	fmt.Printf("%s + %s + %s", tx, t200, t400)
 
-	// Output: image: BMW.M5.200.jpg + image: BMW.M5.400.jpg
+	// Output: image:BMW.M5 + image:BMW.M5@200 + image:BMW.M5@400
 }
 
 func getCarImage(manufacturer, model string) []byte {
 	// In the real world this would go off and fetch an actual image.
-	return []byte(fmt.Sprintf("image: %s.%s", manufacturer, model))
+	return []byte(fmt.Sprintf("image:%s.%s", manufacturer, model))
 }
 
 func resize(image []byte, size int) []byte {
 	// In the real world this would resize the image.
-	return []byte(fmt.Sprintf("%s.%d.jpg", image, size))
+	return []byte(fmt.Sprintf("%s@%d", image, size))
 }
